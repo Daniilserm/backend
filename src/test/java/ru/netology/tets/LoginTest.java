1b2b075ef407b691a84b9592ba9038e8b247779a
Dan Serm <d.barshinov@yandex.ru>
2023-10-31 14:25:03 +0300

    add dependencies

--- src/test/java/ru/netology/tets/LoginTest.java
+++ src/test/java/ru/netology/tets/LoginTest.java
@@ -1,6 +1,5 @@
 package ru.netology.tets;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -8,23 +7,20 @@ import ru.netology.data.APIHelper;
 import ru.netology.data.DataHelper;
 import ru.netology.data.SQLHelper;
 
-import java.time.Duration;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
 
-//    @AfterEach
-//    void clean() {
-//        SQLHelper.cleanCodes();
-//    }
-//
-//    @AfterAll
-//    static void cleanAll() {
-//        SQLHelper.cleanData();
-//    }
+    @AfterEach
+    void clean() {
+        SQLHelper.cleanCodes();
+    }
+
+    @AfterAll
+    static void cleanAll() {
+        SQLHelper.cleanData();
+    }
 
 
     @Test
@@ -50,27 +46,5 @@ public class LoginTest {
         assertEquals(actualFirstCardBalance, expectedFirstCardBalance);
         assertEquals(actualSecondCardBalance, expectedSecondCardBalance);
     }
-
-
-//    $("[data-test-id=login] input").setValue(registeredUser.getLogin());
-//    $("[data-test-id=password] input").setValue(wrongPassword);
-//    $(byText("Продолжить")).click();
-//    $("[data-test-id=error-notification]")
-//                .shouldHave(Condition.text("Неверно указан логин или пароль"), Duration.ofSeconds(15))
-//            .shouldBe(Condition.visible);
-//
-//    var firstCardInfo = getFirstCardInfo();
-//    var secondCardInfo = getSecondCardInfo();
-//    var firstCardBalance = dashboardPage.getCardBalance(0);
-//    var secondCardBalance = dashboardPage.getCardBalance(1);
-//    var amount = generateValidAmount(secondCardBalance);
-//    var transferPage = dashboardPage.selectCardTransfer(firstCardInfo);
-//    dashboardPage = transferPage.validTransfer(String.valueOf(amount), secondCardInfo);
-//    var actualFirstCardBalance = dashboardPage.getCardBalance(0);
-//    var actualSecondCardBalance = dashboardPage.getCardBalance(1);
-//    var expectedFirstCardBalance = firstCardBalance + amount;
-//    var expectedSecondCardBalance = secondCardBalance - amount;
-//    assertEquals(actualFirstCardBalance, expectedFirstCardBalance);
-//    assertEquals(actualSecondCardBalance, expectedSecondCardBalance);
 
 }
